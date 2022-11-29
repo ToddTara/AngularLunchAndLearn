bac57bea281d7e5beb637c53b112a54397cbc5a4
ToddTara <93586928+ToddTara@users.noreply.github.com>
2022-11-29 16:33:20 +0000

    remove code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { MenuItem } from "primeng/api";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'myNewAngularApp';
-  menu: MenuItem[] = [];
-
-  ngOnInit() {
-    this.menu = [
-      {
-        label: 'HTML',
-        items: [
-          {
-            label: 'HTML 1'
-          },
-          {
-            label: 'HTML 2'
-          }
-        ]
-      },
-      {
-        label: 'Angular',
-
-        items: [
-          {
-            label: 'Angular 1'
-          },
-          {
-            label: 'Angular 2'
-          }
-        ]
-      }
-    ];
-  }
 }
